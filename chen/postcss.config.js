14b1c6dbbcf10098a503d40e2da20426109d1a8d
tu pham <shammer280@gmail.com>
2020-08-17 12:44:22 +0700

    first commit

---
+++ chen/postcss.config.js
@@ -0,0 +1,16 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+
+module.exports = {
+  plugins: [
+    require("tailwindcss")("./tailwind.config.js"),
+    require("autoprefixer"),
+    cssnano({
+      preset: "default",
+    }),
+    purgecss({
+      content: ["./*.html", "./src/**/*.js"],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+  ],
+};
